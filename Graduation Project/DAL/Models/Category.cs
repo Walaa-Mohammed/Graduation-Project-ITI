9b5ad6a5dd893d2f8f1724b3db5988094c3049f9
Walaa Mohamed <wm205mohamed@gmail.com>
2021-05-28 00:38:27 +0200

    Create Models

---
+++ Graduation Project/DAL/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Category
+    {
+
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public virtual List<SubCategory> SubCategories { get; set; }
+
+    }
+}
